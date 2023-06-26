5be12680153f2a0dcf7e97fe4d4552e866f4cd3d
Samarth <94822738+SamarthKumar3@users.noreply.github.com>
2023-06-26 21:09:44 +0530

    changes made from vscode

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch , currency} = useContext(AppContext);
+    const [budgetValue, setBudgetValue] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        const newBudgetValue = event.target.value;
+        setBudgetValue(newBudgetValue);
+        dispatch({ type: 'SET_BUDGET', payload: newBudgetValue });
+    };
+
+    
+    if (budgetValue > 20000) {
+        alert("max value of budget is 20000!");
+    }
+    
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget:{currency}</span>
+            <input type="number" value={budgetValue} step="10" onChange={handleBudgetChange} />
         </div>
     );
 };
